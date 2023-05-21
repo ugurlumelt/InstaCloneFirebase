53c7d8d6c14ca5b46d8e03fd55450c1fead50b95
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-05-21 16:33:42 +0300

    firebase added

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func singInClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+    }
+    
+    @IBAction func singUpClicked(_ sender: Any) {
+    }
 }
 
